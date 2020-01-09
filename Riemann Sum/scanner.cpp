927bca740f6bcd4eb16af3a9e868669087fb50cf
Jorge Mata <juniormata@verizon.net>
2020-01-09 09:18:04 -0800

    Added a header and cpp file for scanner tool

---
+++ Riemann Sum/scanner.cpp
@@ -0,0 +1,9 @@
+//
+//  scanner.cpp
+//  Riemann Sum
+//
+//  Created by Jorge Mata Jr on 1/9/20.
+//  Copyright © 2020 Mata Works. All rights reserved.
+//
+
+#include "scanner.hpp"
---
+++ Riemann Sum/scanner.hpp
@@ -0,0 +1,13 @@
+//
+//  scanner.hpp
+//  Riemann Sum
+//
+//  Created by Jorge Mata Jr on 1/9/20.
+//  Copyright © 2020 Mata Works. All rights reserved.
+//
+
+#pragma once
+
+#include <stdio.h>
+
+
